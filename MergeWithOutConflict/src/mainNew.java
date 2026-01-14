810f88378fab0cdc2d1f3f22dca3ed85c90ed307
rous3530 <20253ds091@utez.edu.mx>
2026-01-14 17:20:58 -0600

    feat: agregar operaciones

--- MergeWithOutConflict/src/mainNew.java
+++ MergeWithOutConflict/src/mainNew.java
@@ -2,8 +2,8 @@
 public class mainNew {
     public static void main(String[] args) {
 
-        System.out.printf("Semana 2");
-        System.out.printf("“Versión base”");
+        System.out.printf("2 + 2 = 4");
+        System.out.printf("5 * 3 = 15");
 
 
     }
