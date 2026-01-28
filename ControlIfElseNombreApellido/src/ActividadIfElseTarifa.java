481c8285ab3a183a68b336a9eea41252954fe868
rous3530 <20253ds091@utez.edu.mx>
2026-01-28 08:45:10 -0600

    Actividad realizada en clase if

---
+++ ControlIfElseNombreApellido/src/ActividadIfElseTarifa.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+public class ActividadIfElseTarifa {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int edad,tarifaFinal;
+        boolean esEstudiante;
+
+
+
+        edad = obtenerInt(sc, "Ingrese su edad: ");
+
+        if (edad < 0 || edad > 120) {
+            System.out.println("Edad inválida");
+            return;
+        }
+
+        esEstudiante = obtenerBoolean(sc, "Es usted estudiante (Sí/No): ");
+
+
+        tarifaFinal = calcularTarifa(edad, esEstudiante);
+
+        mostrarResumen(edad, esEstudiante, tarifaFinal);
+    }
+
+    /**
+     * Calcula la tarifa basada en la edad y el estatus de estudiante.
+     * @param edad Edad del usuario.
+     * @param esEstudiante Booleano que indica si es estudiante.
+     * @return El costo de la tarifa.
+     */
+    public static int calcularTarifa(int edad, boolean esEstudiante) {
+        int tarifa = 0;
+
+        if (edad < 12) {
+            tarifa = 50;
+        } else if (edad >= 12 && edad <= 17) {
+            if (esEstudiante) {
+                tarifa = 60;
+            } else {
+                tarifa = 80;
+            }
+        } else if (edad >= 18) {
+            if (esEstudiante) {
+                tarifa = 90;
+            } else {
+                tarifa = 120;
+            }
+        }
+
+        return tarifa;
+    }
+
+    /**
+     * Solicita un entero al usuario.
+     * @param sc Scanner para la entrada.
+     * @param mensaje Mensaje a mostrar.
+     * @return El entero ingresado.
+     */
+    public static int obtenerInt(Scanner sc, String mensaje) {
+        System.out.print(mensaje);
+        return sc.nextInt();
+    }
+
+    /**
+     * Solicita un booleano al usuario.
+     * @param sc Scanner para la entrada.
+     * @param mensaje Mensaje a mostrar.
+     * @return El booleano ingresado.
+     */
+    public static boolean obtenerBoolean(Scanner sc, String mensaje) {
+        System.out.print(mensaje);
+        return sc.nextBoolean();
+    }
+
+    /**
+     * Muestra los datos finales en pantalla.
+     * @param edad Edad ingresada.
+     * @param esEstudiante Estatus de estudiante.
+     * @param tarifa Tarifa calculada.
+     */
+    public static void mostrarResumen(int edad, boolean esEstudiante, int tarifa) {
+        System.out.println("\nResumen de Tarifa");
+        System.out.println("Edad ingresada: " + edad);
+        System.out.println("Es estudiante: " + (esEstudiante ? "Sí" : "No"));
+        System.out.println("Tarifa final: $" + tarifa);
+    }
+}
--- ControlSwitchEduardoFlores/src/ActividadSwitchCalculadora.java
+++ ControlSwitchEduardoFlores/src/ActividadSwitchCalculadora.java
@@ -44,28 +44,28 @@ public class ActividadSwitchCalculadora {
                 valorA = optenerDouble(sc,"Ingrese su primer numero a sumar: ");
                 valorB = optenerDouble(sc,"Ingrese su segundo digito que sumara: ");
                 double operacionSuma = calcuclarSuma(valorA,valorB);
-                System.out.println("El resultado de sumar "+valorA+"+"+valorB+operacionSuma);
+                System.out.println("El resultado de sumar "+valorA+"+"+valorB+"="+operacionSuma);
                 break;
             case 2:
                 System.out.println("Selecciono Resta");
                 valorA = optenerInt(sc,"Ingrese su primer numero a restar: ");
                 valorB = optenerInt(sc,"Ingrese su segundo numero que restara: ");
                 double operacionResta= calcularResta(valorA,valorB);
-                System.out.println("El resultado de restar "+valorA+"-"+valorB+operacionResta);
+                System.out.println("El resultado de restar "+valorA+"-"+valorB+"="+operacionResta);
                 break;
             case 3:
                 System.out.println("Selecciono Multiplicacion");
                 valorA = optenerInt(sc,"Ingrese su primer numero a multiplicar: ");
                 valorB = optenerInt(sc,"Ingrese su segundo numero que multiplicara: ");
                 double operacionMultiplicar = calcularMultiplicacion(valorA,valorB);
-                System.out.println("El resultado de muliplicar "+valorA+"*"+valorB+operacionMultiplicar);
+                System.out.println("El resultado de muliplicar "+valorA+"*"+valorB+"="+operacionMultiplicar);
                 break;
             case 4:
                 System.out.println("Selecciono Division ");
                 valorA = optenerInt(sc,"Ingrese su primer numero a divisor: ");
                 valorB = optenerInt(sc,"Ingrese su segundo numero que sera el dividendo: ");
                 double operacionDivision = calcularDivision(valorA,valorB);
-                System.out.println("El resultado de su divir "+valorA+"/"+valorB+operacionDivision);
+                System.out.println("El resultado de su divir "+valorA+"/"+valorB+"="+operacionDivision);
                 break;
             default:
                 System.out.println("Porfavor seleccione una opción valida");
